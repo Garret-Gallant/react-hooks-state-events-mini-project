cb6eae97d6dd03ca14ee981f489133c580d4a37c
Garret Gallant <gallanttechnological@gmail.com>
2022-07-26 02:01:23 -0600

    lesson complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [taskArray, setTaskArray] = useState(TASKS);
+  const [filteredTasks, setFilteredTasks] = useState(TASKS);
+  const [currentFilter, setCurrentFilter] = useState("All");
+
+  useEffect(() => {
+    if (currentFilter === "All") {
+      setFilteredTasks(taskArray);
+    } else {
+      const newArray = taskArray.filter((task) => {
+        return task.category === currentFilter;
+      });
+      setFilteredTasks(newArray);
+    }
+  }, [currentFilter, taskArray]);
+
+  function deleteTask(text) {
+    const newArray = taskArray.filter((task) => task.text !== text);
+    setTaskArray(newArray);
+  }
+
+  function addTask(text, category) {
+    const newTask = {
+      text: text,
+      category: category,
+    };
+
+    setTaskArray([...taskArray, newTask]);
+  }
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter
+        categories={CATEGORIES}
+        filterByCategory={setCurrentFilter}
+      />
+      <NewTaskForm addTask={addTask} categories={CATEGORIES} />
+      <TaskList deleteTask={deleteTask} tasks={filteredTasks} />
     </div>
   );
 }
--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { isCompositeComponent } from "react-dom/test-utils";
 
-function NewTaskForm() {
+function NewTaskForm({ addTask, categories }) {
+  const [text, setText] = useState("");
+  const [category, setCategory] = useState("Code");
+  
   return (
-    <form className="new-task-form">
+    <form
+      className="new-task-form"
+      onSubmit={(e) => {
+        e.preventDefault();
+        addTask(text, category);
+      }}
+    >
       <label>
         Details
-        <input type="text" name="text" />
+        <input
+          type="text"
+          name="text"
+          onChange={(e) => setText(e.target.value)}
+        />
       </label>
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select onChange={(e) => setCategory(e.target.value)} name="category">
+          {categories.map((category) => {
+            return category === "All" ? null : <option>{category}</option>;
+          })}
         </select>
       </label>
       <input type="submit" value="Add task" />
